523c19c0594a7b3bcc5cf47dced49db796d70469
FabriceBosshard <f.bosshard@bosshard.com>
2016-11-18 17:11:27 +0100

    Add files via upload

---
+++ SpaceInvaders/SpaceInvaders/KeyHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SpaceInvaders
+{
+    class KeyHandler
+    {
+        private Player man = null;
+        public void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    if (man.previousDirection != (int)Player.Direction.Right)
+                        man.direction = (int)Player.Direction.Left;
+                    break;
+                case Key.Right:
+                    if (man.previousDirection != (int)Player.Direction.Left)
+                        man.direction = (int)Player.Direction.Right;
+                    break;
+            }
+            man.previousDirection = man.direction;
+        }
+    }
+}
---
+++ SpaceInvaders/SpaceInvaders/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SpaceInvaders
+{
+    class Player
+    {
+        public int direction = 0;
+        public int previousDirection = 0;
+        public Point currentPosition;
+
+        public enum Direction : byte
+        {
+            Up = 1,
+            Down = 2,
+            Left = 3,
+            Right = 4
+        };
+
+        public void shoot()
+        {
+        }
+
+        public Player(Point p)
+        {
+
+        }
+    }
+}
